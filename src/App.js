31737b4d82b972b12b3be9931e71889ad88d209a
Mitch Stonehocker <mitch@aitoconsulting.com>
2019-04-12 12:30:43 -0400

    initial upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+ const App = () => {
+  return (
+    <div className="App">
+      <header className='App-header'>header</header>
+      <main className='App-main'>main</main>
+      <footer className='App-footer'>footer</footer>
+    </div>
+  )
+}
+
+export default App
